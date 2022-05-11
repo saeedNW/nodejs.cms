8cdda1353ed0e50133ae4fe0efc89d25ae996612
Saeed Noroozi <saeednorouzi98@gmail.com>
2022-05-11 10:31:39 +0430

    change order of new course validation roles.

--- app/controllers/admin/validator/newCourseValidator.js
+++ app/controllers/admin/validator/newCourseValidator.js
@@ -5,10 +5,20 @@ const {PaymentType} = require("../../../constants").coursesConstants;
 /** import courses model */
 const {courseModel} = require("../../../models").model;
 
+/** define array variable for image mimetype */
+const mimetype = ["image/jpeg", "image/png", "image/jpg", "image/svg"];
 
 exports.newCourseValidator = yup.object().shape({
     title: yup.string()
         .min(5, "عنوان نمیتواند کمتر از 5 کاراکتر باشد"),
+    paymentType: yup.string()
+        .oneOf(Object.values(PaymentType), "لطفا یکی از مقادیر مشخص شده را برای نوع دوره انتخاب کنید"),
+    description: yup.string()
+        .min(20, "توضیحات نمیتواند کمتر از 20 کاراکتر باشد"),
+    price: yup.string()
+        .required("فیلد قیمت نمیتواند خالی باشد"),
+    tags: yup.string()
+        .required("فیلد تگ ها نمیتواند خالی باشد"),
     slug: yup.string()
         .min(5, "نامک نمیتواند کمتر از 5 کاراکتر باشد")
         .test(
@@ -23,12 +33,7 @@ exports.newCourseValidator = yup.object().shape({
                 return !findSlug;
             }
         ),
-    paymentType: yup.string()
-        .oneOf(Object.values(PaymentType), "لطفا یکی از مقادیر مشخص شده را برای نوع دوره انتخاب کنید"),
-    description: yup.string()
-        .min(20, "توضیحات نمیتواند کمتر از 20 کاراکتر باشد"),
-    price: yup.string()
-        .required("فیلد قیمت نمیتواند خالی باشد"),
-    tags: yup.string()
-        .required("فیلد تگ ها نمیتواند خالی باشد")
+    images: yup.object().shape({
+
+    })
 });
