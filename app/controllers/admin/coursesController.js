d6337ad28588b1efd25e914d33be53299f21e465
Saeed Noroozi <saeednorouzi98@gmail.com>
2022-05-13 03:37:14 +0430

    add multiply image creation process from single uploaded image.

--- app/controllers/admin/coursesController.js
+++ app/controllers/admin/coursesController.js
@@ -10,6 +10,10 @@ const {getHashId} = require("../../core/getHashId");
 const {identifierModels} = require("../../constants").identifierConstants;
 /** import file system module */
 const fs = require("fs");
+/** import path module */
+const path = require("path");
+/** import sharp module */
+const sharp = require("sharp");
 
 /** import main controller file */
 const Controller = require("../controller");
@@ -58,12 +62,18 @@ class CoursesController extends Controller {
                 return this.redirectURL(req, res);
             }
 
-            /** image uploader */
-            this.createImageLink(req);
+            /**
+             * image uploader.
+             * use createImageURL method if you planed to use only one original uploaded image.
+             * use imageResize method if you planed to create multiply images from uploaded image.
+             */
+            this.imageResize(req, next);
+            // this.createImageURL(req, next);
 
             /** create new course */
             await this.createCourse(req, res, next);
         } catch (err) {
+            console.log(err)
             next("فرایند با مشکل مواجه شد لطفا مجددا تلاش نمایید");
         }
     }
@@ -123,6 +133,7 @@ class CoursesController extends Controller {
             /** return user to the courses main page */
             res.redirect("/admin/panel/courses");
         } catch (err) {
+            console.log(err)
             next("فرایند با مشکل مواجه شد لطفا مجددا تلاش نمایید");
         }
     }
@@ -147,20 +158,115 @@ class CoursesController extends Controller {
 
             res.json({message: "نامک قابل استفاده می باشد"});
         } catch (err) {
+            console.log(err)
             res.status(err.status).json({...err.message});
         }
     }
 
     /**
-     * set image path (link) into request body as images
+     * create uploaded image URL
      * @param req
+     * @param next
+     * @param {boolean} multiImages set this to true if you want to create multiply images from original image
+     * @param imageName image name for multiply images
      */
-    createImageLink(req) {
-        /** extract image path from request file */
-        let {path} = req.file;
+    createImageURL(req, next, multiImages = false, imageName = null) {
+        try {
+            /** extract file from request file */
+            let file = req.file;
 
-        /** set images path in request body */
-        req.body.images = path.slice(6);
+            /**
+             * extract image info from file.
+             * use for multiply images creation process
+             * @type {ParsedPath}
+             */
+            const imageInfo = path.parse(file.path);
+
+            /**
+             * set images URL in request body.
+             * if you need only one original uploaded image
+             */
+            if (!multiImages)
+                return req.body.images = file.path.slice(6);
+
+            /**
+             * return image URL.
+             * if you planed to use multiply images
+             * EX => /uploads/images/.../2022/5/12/imageOriginalName-720.png
+             */
+            return `${imageInfo.dir.slice(6)}/${imageName}`;
+        } catch (err) {
+            console.log(err);
+            next("فرایند با مشکل مواجه شد لطفا مجددا تلاش نمایید");
+        }
+    }
+
+    /**
+     * multiply images creation process
+     * @param req
+     * @param next
+     */
+    imageResize(req, next) {
+        try {
+            /** extract file from request file */
+            const file = req.file
+
+            /**
+             * extract image info from file.
+             * use for multiply images creation
+             * @type {ParsedPath}
+             */
+            const imageInfo = path.parse(file.path);
+
+            /**
+             * this object will be used to save multiply images addresses
+             * @type {any}
+             */
+            let imagesAddress = {};
+
+            /**
+             * add original image URL to imagesAddress object
+             * @type {*|string}
+             */
+            imagesAddress.orginal = this.createImageURL(req, next, true, file.filename);
+
+            /**
+             * creating multiply files from original file.
+             * new files resolution will be coming from environment variable.
+             * default resolutions are 1080, 720 and 480.
+             */
+            JSON.parse(process.env.IMAGES_SIZE).map(async (size) => {
+                /**
+                 * set image name.
+                 * EX => imageOriginalName-720.png
+                 * @type {string}
+                 */
+                let imageName = `${imageInfo.name}-${size}${imageInfo.ext}`;
+
+                /**
+                 * add new image URL to imagesAddress object.
+                 * EX => /uploads/images/.../2022/5/12/imageOriginalName-720.png
+                 * @type {*|string}
+                 */
+                imagesAddress[size] = this.createImageURL(req, next, true, imageName);
+
+                /**
+                 * resize original uploaded image to given resolution
+                 * and upload new image
+                 */
+                await sharp(file.path)
+                    .resize(size, null)
+                    .toFile(`${file.destination}/${imageName}`)
+            })
+
+            /**
+             * set imagesAddress object in request body
+             */
+            req.body.images = imagesAddress;
+        } catch (err) {
+            console.log(err)
+            next("فرایند با مشکل مواجه شد لطفا مجددا تلاش نمایید");
+        }
     }
 }
 
