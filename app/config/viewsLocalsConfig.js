a8efcb1bf9295f6d395e1ecdbaa3afc4d5b2ba91
Saeed Noroozi <saeednorouzi98@gmail.com>
2022-05-28 13:32:54 +0430

    add date converter.

--- app/config/viewsLocalsConfig.js
+++ app/config/viewsLocalsConfig.js
@@ -2,6 +2,10 @@
 const Config = require("./config");
 /** import path module */
 const path = require("path");
+/** import moment-jalaali module */
+const moment = require("moment-jalaali");
+/** activate persian language for moment */
+moment.loadPersian({usePersianDigits: true})
 
 /**
  * session configs
@@ -29,7 +33,8 @@ module.exports = class ViewsLocalsConfig extends Config {
             viewDir: this.viewPath,
             errors: this.req.flash("errors"),
             success: this.req.flash("success"),
-            oldData: this.oldInfo
+            oldData: this.oldInfo,
+            convertDate: this.convertDate
         }
     }
 
@@ -63,65 +68,13 @@ module.exports = class ViewsLocalsConfig extends Config {
     oldInfo(field, defaultValue = '') {
         return this.formData && this.formData.hasOwnProperty(field) ? this.formData[field] : defaultValue;
     }
-}
 
-/*
-/!**
- * this variable will be used to set forms inputs data to return
- * them to front if there was any error during any process that have
- * a form involved
- *!/
-let formData;
-
-/!**
- * config global values for ejs views
- * @param app
- *!/
-exports.viewsGlobalInfo = (app) => {
-    app.use((req, res, next) => {
-        /!** set form data *!/
-        formData = req.flash("formData")[0];
-
-        /!** set ejs view engin local values *!/
-        app.locals = {
-            auth: this.userData(req),
-            viewDir: this.viewPath,
-            errors: req.flash("errors"),
-            success: req.flash("success"),
-            oldData: this.oldInfo
-        };
-        next();
-    });
-}
-
-/!**
- * send back user data if exists
- * @return {{loginCheck: boolean, user}}
- *!/
-exports.userData = (req) => {
-    return {
-        user: req.user,
-        loginCheck: req.isAuthenticated()
+    /**
+     * date converter method
+     * @param time
+     * @return {*|moment.Moment}
+     */
+    convertDate(time) {
+        return moment(time)
     }
 }
-
-/!**
- * this method is used to return specific address in views directory
- * @param dir desired directory
- * @return {string}
- *!/
-exports.viewPath = (dir) => {
-    return path.resolve(`./resource/views/${dir}`);
-}
-
-/!**
- * this method is used to set and return form data to front-end as
- * a local variable of the view engine
- * @param field input name
- * @param defaultValue input default value
- * @return {*|string}
- *!/
-exports.oldInfo = (field, defaultValue = '') => {
-    return formData && formData.hasOwnProperty(field) ? formData[field] : defaultValue;
-}*/
-
